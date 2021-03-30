e6f10ff11cbbe77b693058eb31e0acc262129e9e
Maxwell Schaefer <max@theschaefers.com>
2021-03-30 14:59:51 -0700

    Removed default content

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Maxwell Schaefer
         </p>
         
       </header>
+      <div className="App-body">
+        <p>Hi</p>
+      </div>
     </div>
   );
 }
